7c7709fb8c18465fde91997a71debb40a1de98cd
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-08 15:37:22 +0530

    adding multi image filter on product detail page

--- shop/static/js/product-detail.js
+++ shop/static/js/product-detail.js
@@ -1,17 +1,18 @@
 // $(document).ready(function() {
 //     const thumbnailData = $(".thumbnail");
 //     const activeItem = $(".active");
-//     console.log(thumbnailData, activeItem);
 
-//     // for (let index = 0; index < thumbnailData.length; index++) {
-//     //     thumbnailData[index].mouseover(function() {
-//     //         if (activeItem.length > 0) {
-//     //             activeItem[0].removeClass("active");
-//     //         }
+//     for (var i = 0; i < thumbnailData.length; i++) {
+//         const element = thumbnailData[i];
 
-//     //         this.addClass("active");
-//     //         console.log("this --> ", this);
-//     //         $(".product-img").attr("src", this.attr("src"));
-//     //     });
-//     // }
+//         element.on("mouseover", function(e) {
+//             if (activeItem.length > 0) {
+//                 activeItem[0].removeClass("active");
+//             }
+//             this.addClass("active");
+//             $(".product-img").attr("src", this.attr("src"));
+//         });
+//     }
 // });
+
+$(document).ready(function() {});
