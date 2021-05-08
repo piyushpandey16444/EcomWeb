c8d52c7fb8b4b8304b050a67787bb3bee1098055
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-08 15:14:25 +0530

    adding product detail js files

---
+++ shop/static/js/product-detail.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//     const thumbnailData = $(".thumbnail");
+//     const activeItem = $(".active");
+//     console.log(thumbnailData, activeItem);
+
+//     // for (let index = 0; index < thumbnailData.length; index++) {
+//     //     thumbnailData[index].mouseover(function() {
+//     //         if (activeItem.length > 0) {
+//     //             activeItem[0].removeClass("active");
+//     //         }
+
+//     //         this.addClass("active");
+//     //         console.log("this --> ", this);
+//     //         $(".product-img").attr("src", this.attr("src"));
+//     //     });
+//     // }
+// });
