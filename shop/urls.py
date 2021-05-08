b6b339395472c5a6df0228c28463ed8a97951010
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-08 14:44:19 +0530

    adding product-detail page

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('product/', product_detail_view, name='product-detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'shop/home.html')
+
+
+def product_detail_view(request):
+    return render(request, 'shop/product-detail.html')
