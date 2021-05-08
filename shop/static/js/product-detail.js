dfa012da352fc99ebb25f6558f1ff6791171659b
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-08 20:14:54 +0530

    added thumbnail img in product main page

--- shop/static/js/product-detail.js
+++ shop/static/js/product-detail.js
@@ -1,18 +1,9 @@
-// $(document).ready(function() {
-//     const thumbnailData = $(".thumbnail");
-//     const activeItem = $(".active");
-
-//     for (var i = 0; i < thumbnailData.length; i++) {
-//         const element = thumbnailData[i];
-
-//         element.on("mouseover", function(e) {
-//             if (activeItem.length > 0) {
-//                 activeItem[0].removeClass("active");
-//             }
-//             this.addClass("active");
-//             $(".product-img").attr("src", this.attr("src"));
-//         });
-//     }
-// });
-
-$(document).ready(function() {});
+$(document).ready(function() {
+    $(".thumbnail").hover(function(e) {
+        e.preventDefault();
+        const self = this;
+        $(".active").removeClass("active");
+        $(self).addClass("active");
+        $(".product-img").attr("src", $(self).attr("src"));
+    });
+});
