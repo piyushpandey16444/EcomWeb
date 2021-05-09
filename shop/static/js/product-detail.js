1ba8ea5ebfc4b315c9def2b9763b61d3ff685990
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-09 16:39:37 +0530

    adding breadcrumb to detail page

--- shop/static/js/product-detail.js
+++ shop/static/js/product-detail.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
+
     $(".thumbnail").hover(function(e) {
         e.preventDefault();
         const self = this;
         $(".active").removeClass("active");
+        $(".hover__on").removeClass("hover__on");
         $(self).addClass("active");
+        $(self).addClass("hover__on");
         $(".product-img").attr("src", $(self).attr("src"));
-    });
-
-    $(".upper__arrow").click(() => {
-        console.log("done");
-        $(".upper__arrow").scrollTop(180);
-    });
-
-    $(".lower__arrow").click((e) => {
-        e.preventDefault();
-        $(".lower__arrow").scrollDown(180);
     });
 });
