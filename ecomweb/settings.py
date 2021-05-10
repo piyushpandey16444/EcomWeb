c97f92efa7da5c49a55bc5e10c017ff9688a5cf8
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-10 23:35:04 +0530

    postgresql database connection made

--- ecomweb/settings.py
+++ ecomweb/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'ecomweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# POSTGRES_CONNECTION
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EcomWeb',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'piyush',
+        'PASSWORD': 'G@ecsi22a'
     }
 }
 
