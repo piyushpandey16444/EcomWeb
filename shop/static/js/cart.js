a7bcee32c630a73fe5efb6a9c592c6a3e0dba241
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-05-15 16:21:36 +0530

    cart function added

--- shop/static/js/cart.js
+++ shop/static/js/cart.js
@@ -1,1 +1,22 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+    $(".up__icon").click(function(e) {
+        e.preventDefault();
+        const getQty = $(this).attr("value");
+        const n = $("#item-`getQty`");
+        console.log(n);
+
+        const cartData = {
+            item_id: getQty,
+        };
+        $.ajax({
+            type: "POST",
+            url: "/update-qty/",
+            data: JSON.stringify(cartData),
+            dataType: "dataType",
+            success: function(response) {
+                const n = $("#item-`getQty`");
+                console.log(n);
+            },
+        });
+    });
+});
